f98d8b32efb08e61bbfd79006f541c12398e7bed
theoklahuman <theoklahuman@gmail.com>
2022-12-23 21:54:59 -0600

    Make page optimized for bigger screen

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const title = document.querySelector("title");
 const main = document.querySelector("main");
 const submitButton = document.querySelector(".submit-button");
 const ratingFinalized = document.querySelector(".rating-finalized");
+const ratings = document.querySelector(".ratings-container");
+const ratingMessage = document.querySelector(".rating-summary-message");
+
+let userRating;
 
 function showRating() {
-    main.style.display = "none";
-    ratingFinalized.style.display = "block";
-
+  main.style.display = "none";
+  ratingFinalized.style.display = "block";
+  title.textContent = "Thank you!";
+  ratingMessage.textContent = `You selected ${userRating} out of 5`;
 }
+
+ratings.addEventListener("click", (event) => {
+  userRating = event.target.textContent;
+  return userRating;
+});
 
 submitButton.addEventListener("click", showRating);
